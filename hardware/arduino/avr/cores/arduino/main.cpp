6a89dbae8fd96ed2ba01db94f2d6128b6c2ff069
Phillip Stevens <phillip.stevens@gmail.com>
2015-11-25 17:37:30 +1100

    Weak attribute cannot be overwritten, if function is in same file.
    The weak attribute __attribute((weak)) cannot be overwritten by the
    linker if the function is defined in the same file that it is used.
    
    Weak functions need to be moved to another file (hooks.c) to enable them
    to be overwritten by libraries.

--- hardware/arduino/avr/cores/arduino/main.cpp
+++ hardware/arduino/avr/cores/arduino/main.cpp
@@ -19,14 +19,6 @@
 // Declared weak in Arduino.h to allow user redefinitions.
 int atexit(void (* /*func*/ )()) { return 0; }
 
-// Weak empty variant initialization function.
-// May be redefined by variant files.
-void initVariant() __attribute__((weak));
-void initVariant() { }
-
-void setupUSB() __attribute__((weak));
-void setupUSB() { }
-
 /* Main program loop, never return from main() */
 int main(void) __attribute__((OS_main));
 int main(void)
